7deb5c93a4a58decdfd2265c978012466697972d
Quantumfishcake <38376693+Quantumfishcake@users.noreply.github.com>
2018-05-27 17:31:50 +1000

    created game logic

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
+
+
+console.log('user fliped' + ' ' + cardOne)
+console.log('user fliped' + ' ' + cardTwo)
